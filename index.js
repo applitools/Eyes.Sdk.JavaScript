f334643e154bbe04ff3065ffeb37c6193056c201
applitools <team@applitools.com>
2014-07-20 16:13:14 +0300

    Adapted the SDK package to the new usage by creating an index file to expose the different modules.

---
+++ index.js
@@ -0,0 +1,6 @@
+exports.EyesBase = require('./src/EyesBase');
+exports.EyesPromiseFactory = require('./src/EyesPromiseFactory');
+exports.GeneralUtils = require('./src/GeneralUtils');
+exports.MatchWindowTask = require('./src/MatchWindowTask');
+exports.ScreenshotTaker = require('./src/ScreenshotTaker');
+exports.ServerConnector = require('./src/ServerConnector');
