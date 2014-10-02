3416d3c15b5202feb1fe129e582d44665d10ee67
Daniel Puterman <daniel.puterman@applitools.com>
2014-10-02 21:04:20 +0300

    Updated agent Id.

--- src/Eyes.js
+++ src/Eyes.js
@@ -40,7 +40,7 @@
 
     //noinspection JSUnusedGlobalSymbols
     Eyes.prototype._getBaseAgentId = function () {
-        return 'eyes.images/0.0.5';
+        return 'eyes.images/0.0.7';
     };
 
     /**
