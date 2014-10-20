d4daba00f28ddfebf7db1d869d9f7fbc9343ce1f
Daniel Puterman <daniel.puterman@applitools.com>
2014-10-20 12:10:23 +0300

    Exported ImageUtils.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 exports.EyesBase = require('./src/EyesBase');
 exports.EyesPromiseFactory = require('./src/EyesPromiseFactory');
 exports.GeneralUtils = require('./src/GeneralUtils');
+exports.ImageUtils = require('./src/ImageUtils');
 exports.MatchWindowTask = require('./src/MatchWindowTask');
 exports.ScreenShotTaker = require('./src/ScreenShotTaker');
 exports.ServerConnector = require('./src/ServerConnector');
