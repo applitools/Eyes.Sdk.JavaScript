16bff72c1291b7a3b37b46dfa4e7a6e2cc7106cf
Daniel Puterman <daniel.puterman@applitools.com>
2014-10-21 12:54:45 +0300

    Exported GeometryUtils

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ var EyesSDK = require('eyes.sdk');
 exports.ConsoleLogHandler = EyesSDK.ConsoleLogHandler;
 exports.NullLogHandler  = EyesSDK.NullLogHandler;
 exports.ImageUtils = EyesSDK.ImageUtils;
+exports.GeometryUtils = EyesSDK.GeometryUtils;
