bbb5b40eb03a23a74da6b8e263407bfd22e7008c
danielputerman <daniel@dapt1.local>
2015-04-14 20:37:41 +0300

    Added coordinates to MutableImage + eyes.utils rev. up.

--- src/MutableImage.js
+++ src/MutableImage.js
@@ -61,7 +61,6 @@
     }
 
     /**
-     *
      * C'tor = initializes the module settings
      *
      * @param {Buffer} imageBuffer
@@ -75,7 +74,40 @@
         this._imageBmp = undefined;
         this._width = 0;
         this._height = 0;
+        this._top = 0;
+        this._left = 0;
     }
+
+    /**
+     * Coordinates represent the image's position in a larger context (if any). E.g., A screenshot of the browser's
+     * viewport of a web page.
+     *
+     * @return {Promise} A promise which resolves to the coordinates of the image in the larger
+     *                  context (if any): {top: *, left: *}
+     */
+    MutableImage.prototype.getCoordinates = function () {
+        var that = this;
+        return _parseImage(that).then(function () {
+            return {
+                left: that._left,
+                top: that._top
+            };
+        });
+    };
+
+    /**
+     * Coordinates represent the image's position in a larger context (if any). E.g., A screenshot of the browser's
+     * viewport of a web page.
+     *
+     * @return {Promise} A promise which resolves once the set is done.
+     */
+    MutableImage.prototype.setCoordinates = function (coordinates) {
+        var that = this;
+        return _parseImage(that).then(function () {
+            that._left = coordinates.left;
+            that._top = coordinates.top;
+        });
+    };
 
     //noinspection JSUnusedGlobalSymbols
     /**
@@ -116,10 +148,10 @@
 
     //noinspection JSUnusedGlobalSymbols
     /**
-     * scales the image (used to downsize by 2 for retina display chrome bug - and tested accordingly).
+     * Scales the image in place (used to downsize by 2 for retina display chrome bug - and tested accordingly).
      *
      * @param {Float} scale
-     * @return {Object} promise - resolves without any value
+     * @return {Object} promise - resolves without any value.
      */
     MutableImage.prototype.scaleImage = function (scale) {
         var that = this;
@@ -127,7 +159,7 @@
             .then(function () {
                 if (that._isParsed) {
                     return ImageUtils.scaleImage(that._imageBmp, scale)
-                        .then(function(){
+                        .then(function () {
                             that._width = that._imageBmp.width;
                             that._height = that._imageBmp.height;
                         });
@@ -147,8 +179,17 @@
         return _parseImage(that)
             .then(function () {
                 if (that._isParsed) {
+                    // If the region's coordinates are relative to the image, we convert them to absolute coordinates.
+                    if (region && region.relative) {
+                        region = {
+                            left: region.left - that._left,
+                            top: region.top - that._top,
+                            width: region.width,
+                            height: region.height
+                        };
+                    }
                     return ImageUtils.cropImage(that._imageBmp, region)
-                        .then(function(){
+                        .then(function () {
                             that._width = that._imageBmp.width;
                             that._height = that._imageBmp.height;
                         });
