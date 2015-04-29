e15f1cb9ddaea11ea132c98b9ef9e7d92cc30ae7
Doron Zavelevsky <DoronWorkMac@Dorons-MacBook-Pro.local>
2015-04-29 23:08:49 +0300

    Full page screenshot support

--- src/BrowserUtils.js
+++ src/BrowserUtils.js
@@ -11,6 +11,10 @@
 (function () {
     "use strict";
 
+    var MutableImage = require('./MutableImage');
+    var GeometryUtils = require('./GeometryUtils');
+    var ImageUtils = require('./ImageUtils');
+
     /**
      * Gets the device pixel ratio.
      *
@@ -44,6 +48,12 @@
             var y = parseInt(results[1], 10) || 0;
             return {left: x, top: y};
         });
+    };
+
+    var scrollTo = function (browser, point) {
+        return browser.executeScript(
+            'window.scrollTo(' + parseInt(point.left, 10) + ', ' + parseInt(point.top, 10) + ');'
+        );
     };
 
     /**
@@ -116,13 +126,152 @@
             });
     };
 
+    var _processPart = function (part, parts, imageObj, browser, promise, promiseFactory, sizeFactor) {
+        return promise.then(function() {
+            return promiseFactory.makePromise(function (resolve) {
+                // Skip 0,0 as we already got the screenshot
+                if (part.left == 0 && part.top == 0) {
+                    parts.push({
+                        image: imageObj.imageBuffer,
+                        size: {width: imageObj.width, height: imageObj.height},
+                        position: {left: 0, top: 0}
+                    });
+
+                    resolve();
+                    return;
+                }
+
+                var currentPosition;
+                var partImage;
+
+                scrollTo(browser, {left: part.left / sizeFactor, top: part.top / sizeFactor})
+                    .then(function () {
+                        return browser.controlFlow().timeout(100);
+                    })
+                    .then(function () {
+                        return getCurrentScrollPosition(browser);
+                    })
+                    .then(function (position) {
+                        currentPosition = {left: position.left * sizeFactor, top: position.top * sizeFactor};
+                        return browser.takeScreenshot();
+                    })
+                    .then(function (part64) {
+                        var partImage = new MutableImage(new Buffer(part64, 'base64'), promiseFactory);
+                        return partImage.asObject();
+                    })
+                    .then(function (partObj) {
+                        parts.push({
+                            image: partObj.imageBuffer,
+                            size: {width: partObj.width, height: partObj.height},
+                            position: {left: currentPosition.left, top: currentPosition.top}
+                        });
+
+                        resolve();
+                    });
+            });
+        });
+    };
+
+    var getFullPageScreenshot = function (browser, promiseFactory, viewportSize) {
+        var MAX_SCROLL_BAR_SIZE = 50; // This should cover all scroll bars (and some fixed position footer elements :).
+        var MIN_SCREENSHOT_PART_HEIGHT = 10;
+        var sizeFactor = 1;
+        var originalScrollPosition,
+            entirePageSize,
+            imageObject,
+            screenshot;
+
+        return promiseFactory.makePromise(function (resolve) {
+            return getCurrentScrollPosition(browser)
+                .then(function(point) {
+                    originalScrollPosition = point;
+                    return scrollTo(browser, {left: 0, top: 0});
+                })
+                .then(function () {
+                    return getCurrentScrollPosition(browser);
+                })
+                .then(function(point) {
+                    if (point.left != 0 || point.top != 0) {
+                        throw new Error("Could not scroll to the top/left corner of the screen");
+                    }
+                    return getEntirePageSize(browser);
+                })
+                .then(function(pageSize) {
+                    entirePageSize = pageSize;
+                    // Take screenshot of the 0,0 tile
+                    return browser.takeScreenshot();
+                })
+                .then(function(screenshot64) {
+                    screenshot = new MutableImage(new Buffer(screenshot64, 'base64'), promiseFactory);
+                    return screenshot.asObject();
+                })
+                .then(function(imageObj) {
+                    imageObject = imageObj;
+                    return findImageNormalizationFactor(browser, imageObj, viewportSize);
+                }).then(function(factor) {
+                    if (factor === 0.5) {
+                        sizeFactor = 2;
+                        entirePageSize.width *= sizeFactor;
+                        entirePageSize.height *= sizeFactor;
+                        entirePageSize.width = Math.max(entirePageSize.width, imageObject.width);
+                    }
+
+                    return promiseFactory.makePromise(function (resolve) {
+                        // IMPORTANT This is required! Since when calculating the screenshot parts for full size,
+                        // we use a screenshot size which is a bit smaller (see comment below).
+                        if (imageObject.width >= entirePageSize.width && imageObject.height >= entirePageSize.height) {
+                            resolve();
+                            return;
+                        }
+
+                        // We use a smaller size than the actual screenshot size in order to eliminate duplication
+                        // of bottom scroll bars, as well as footer-like elements with fixed position.
+                        var screenshotPartSize = {
+                            width: imageObject.width,
+                            height: Math.max(imageObject.height - (MAX_SCROLL_BAR_SIZE * sizeFactor),
+                                MIN_SCREENSHOT_PART_HEIGHT * sizeFactor)
+                        };
+
+                        var screenshotParts = GeometryUtils.getSubRegions({
+                            left: 0, top: 0, width: entirePageSize.width,
+                            height: entirePageSize.height
+                        }, screenshotPartSize);
+
+                        var parts = [];
+                        var promise = promiseFactory.makePromise(function (resolve) { resolve();});
+
+                        screenshotParts.forEach(function (part) {
+                            promise = _processPart(part, parts, imageObject, browser, promise, promiseFactory, sizeFactor);
+                        });
+                        promise.then(function () {
+                            return ImageUtils.stitchImage(entirePageSize, parts, promiseFactory);
+                        }).then(function (stitchedBuffer) {
+                            screenshot = new MutableImage(stitchedBuffer, promiseFactory);
+                            resolve();
+                        });
+                    });
+                })
+                .then(function () {
+                    return scrollTo(browser, originalScrollPosition);
+                })
+                .then(function() {
+                    return screenshot.asObject();
+                })
+                .then(function (screenshotObj) {
+                    return resolve(screenshotObj.imageBuffer.toString('base64'));
+                });
+        });
+    };
+
     //noinspection JSLint
     var BrowserUtils = {};
     BrowserUtils.getDevicePixelRatio = getDevicePixelRatio;
     BrowserUtils.getCurrentScrollPosition = getCurrentScrollPosition;
+    BrowserUtils.scrollTo = scrollTo;
     BrowserUtils.getEntirePageSize = getEntirePageSize;
     BrowserUtils.setOverflow = setOverflow;
     BrowserUtils.findImageNormalizationFactor = findImageNormalizationFactor;
+    BrowserUtils.getFullPageScreenshot = getFullPageScreenshot;
     //noinspection JSUnresolvedVariable
     module.exports = BrowserUtils;
 }());
