74aaa78a143552327d18934ae78de58914ad2309
danielputerman <danielputerman@users.noreply.github.com>
2016-02-07 22:53:26 +0200

    Base SDK version up.

--- src/Eyes.js
+++ src/Eyes.js
@@ -43,7 +43,7 @@
 
     //noinspection JSUnusedGlobalSymbols
     Eyes.prototype._getBaseAgentId = function () {
-        return 'eyes.images/0.0.34';
+        return 'eyes.images/0.0.35';
     };
 
     /**
