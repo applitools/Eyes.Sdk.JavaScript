6f0afbcf52ee8a57589ee84b57e6c41097e832bc
Oleh Astappiev <astappev@gmail.com>
2016-08-18 09:40:26 +0200

    Move PositionProvider into eyes.utils from Eyes.Selenium.JS

---
+++ src/PositionProvider.js
@@ -0,0 +1,29 @@
+(function () {
+    "use strict";
+
+    /**
+     * @constructor
+     **/
+    function PositionProvider() {}
+
+    /**
+     *
+     * @return {Promise<{x: number, y: number}>} The current position, or {@code null} if position is not
+     * available.
+     */
+    PositionProvider.prototype.getCurrentPosition = function () {};
+
+    /**
+     * Go to the specified location.
+     * @param {Promise<{x: number, y: number}>} location The position to set.
+     */
+    PositionProvider.prototype.setPosition = function (location) {};
+
+    /**
+     * @return {Promise<{x: number, y: number}>} The entire size of the container which the position is relative to.
+     */
+    PositionProvider.prototype.getEntireSize = function () {};
+
+    module.exports = PositionProvider;
+
+}());
