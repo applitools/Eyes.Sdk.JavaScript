ca8b29121c32623c6f7ec1f4cfe84cd2cb2c2bb1
Daniel Puterman <daniel.puterman@applitools.com>
2018-02-11 12:50:05 +0200

    Version up + dependencies.

--- src/Eyes.js
+++ src/Eyes.js
@@ -54,7 +54,7 @@
 
     //noinspection JSUnusedGlobalSymbols
     Eyes.prototype._getBaseAgentId = function () {
-        return 'eyes.images/0.0.48';
+        return 'eyes.images/0.0.49';
     };
 
     /**
