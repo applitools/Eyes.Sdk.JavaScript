7a954767442e09019eb30067988b1908ee5a14a7
Yonatan Lehman <yonatan.lehman@applitools.com>
2019-02-06 14:50:12 +0200

    jsype fixes

--- packages/eyes.sdk/src/MatchSettings.js
+++ packages/eyes.sdk/src/MatchSettings.js
@@ -31,13 +31,13 @@
     /**
      * Encapsulate threshold settings for the "Exact" match level.
      *
-     * @param {number} [minDiffIntensity=0] The minimum intensity difference of pixel to be considered a change. Valid
+     * @param {number} [minDiffIntensity=0] - The minimum intensity difference of pixel to be considered a change. Valid
      *                                      values are 0-255.
-     * @param {number} [minDiffWidth=0] The minimum width of an intensity filtered pixels cluster to be considered a
+     * @param {number} [minDiffWidth=0] - The minimum width of an intensity filtered pixels cluster to be considered a
      *                                  change. Must be >= 0.
-     * @param {number} [minDiffHeight=0] The minimum height of an intensity filtered pixels cluster to be considered a
+     * @param {number} [minDiffHeight=0] - The minimum height of an intensity filtered pixels cluster to be considered a
      *                                  change. Must be >= 0.
-     * @param {number} [matchThreshold=0] The maximum percentage(!) of different pixels (after intensity, width
+     * @param {number} [matchThreshold=0] - The maximum percentage(!) of different pixels (after intensity, width
      *                                      and height filtering) which is still considered as a match. Valid values
      *                                      are fractions between 0-1.
      * @constructor
@@ -52,7 +52,7 @@
     //noinspection JSUnusedGlobalSymbols
     /**
      *
-     * @param {number} [minDiffIntensity=0] The minimum intensity difference of pixel to be considered a change. Valid
+     * @param {number} [minDiffIntensity=0] - The minimum intensity difference of pixel to be considered a change. Valid
      *                                      values are 0-255.
      */
     ExactMatchSettings.prototype.setMinDiffIntensity = function (minDiffIntensity) {
@@ -62,7 +62,7 @@
     //noinspection JSUnusedGlobalSymbols
     /**
      *
-     * @return {number} The minimum intensity difference of pixel to be considered a change.
+     * @return {number} - The minimum intensity difference of pixel to be considered a change.
      */
     ExactMatchSettings.prototype.getMinDiffIntensity = function () {
         return this._minDiffIntensity;
@@ -71,7 +71,7 @@
     //noinspection JSUnusedGlobalSymbols
     /**
      *
-     * @param {number} [minDiffWidth=0] The minimum width of an intensity filtered pixels cluster to be considered a
+     * @param {number} [minDiffWidth=0] - The minimum width of an intensity filtered pixels cluster to be considered a
      *                                  change. Must be >= 0.
      */
     ExactMatchSettings.prototype.setMinDiffWidth = function (minDiffWidth) {
@@ -81,7 +81,7 @@
     //noinspection JSUnusedGlobalSymbols
     /**
      *
-     * @return {number} The minimum width of an intensity filtered pixels cluster to be considered a change.
+     * @return {number} - The minimum width of an intensity filtered pixels cluster to be considered a change.
      */
     ExactMatchSettings.prototype.getMinDiffWidth = function () {
         return this._minDiffWidth;
@@ -90,7 +90,7 @@
     //noinspection JSUnusedGlobalSymbols
     /**
      *
-     * @param {number} [minDiffHeight=0] The minimum height of an intensity filtered pixels cluster to be considered a
+     * @param {number} [minDiffHeight=0] - The minimum height of an intensity filtered pixels cluster to be considered a
      *                                  change. Must be >= 0.
      */
     ExactMatchSettings.prototype.setMinDiffHeight = function (minDiffHeight) {
@@ -100,7 +100,7 @@
     //noinspection JSUnusedGlobalSymbols
     /**
      *
-     * @return {number} The minimum width of an intensity filtered pixels cluster to be considered a change.
+     * @return {number} - The minimum width of an intensity filtered pixels cluster to be considered a change.
      */
     ExactMatchSettings.prototype.getMinDiffHeight = function () {
         return this._minDiffHeight;
@@ -109,7 +109,7 @@
     //noinspection JSUnusedGlobalSymbols
     /**
      *
-     * @param {number} [matchThreshold=0] The maximum percentage(!) of different pixels (after intensity, width
+     * @param {number} [matchThreshold=0] - The maximum percentage(!) of different pixels (after intensity, width
      *                                      and height filtering) which is still considered as a match. Valid values
      *                                      are fractions between 0-1.
      */
@@ -120,7 +120,7 @@
     //noinspection JSUnusedGlobalSymbols
     /**
      *
-     * @return {number} The maximum percentage(!) of different pixels (after intensity, width and height filtering)
+     * @return {number} - The maximum percentage(!) of different pixels (after intensity, width and height filtering)
      *                  which is still considered as a match.
      */
     ExactMatchSettings.prototype.getMatchThreshold = function () {
@@ -130,12 +130,12 @@
     /**
      * Encapsulates the match settings for a session.
      *
-     * @param {MatchLevel} matchLevel The "strictness" level to use.
-     * @param {ExactMatchSettings} [exact] Additional threshold parameters when the {@code Exact} match level is used.
+     * @param {MatchLevel} matchLevel - The "strictness" level to use.
+     * @param {ExactMatchSettings} [exact] - Additional threshold parameters when the {@code Exact} match level is used.
      * @param {boolean} [ignoreCaret]
      *
      * @constructor
-     **/
+     */
     function ImageMatchSettings(matchLevel, exact, ignoreCaret) {
         this._matchLevel = matchLevel;
         this._exact = exact || null;
@@ -144,7 +144,7 @@
 
     //noinspection JSUnusedGlobalSymbols
     /**
-     * @return {MatchLevel} The match level to use.
+     * @return {MatchLevel} - The match level to use.
      */
     ImageMatchSettings.prototype.getMatchLevel = function () {
         return this._matchLevel;
@@ -152,7 +152,7 @@
 
     //noinspection JSUnusedGlobalSymbols
     /**
-     * @param {MatchLevel} matchLevel The match level to use.
+     * @param {MatchLevel} matchLevel - The match level to use.
      */
     ImageMatchSettings.prototype.setMatchLevel = function (matchLevel) {
         this._matchLevel = matchLevel;
@@ -160,7 +160,7 @@
 
     //noinspection JSUnusedGlobalSymbols
     /**
-     * @return {ExactMatchSettings} The additional threshold parameters when the {@code Exact} match level is used, if any.
+     * @return {ExactMatchSettings} - The additional threshold parameters when the {@code Exact} match level is used, if any.
      */
     ImageMatchSettings.prototype.getExact = function () {
         return this._exact;
@@ -168,7 +168,7 @@
 
     //noinspection JSUnusedGlobalSymbols
     /**
-     * @param {ExactMatchSettings|null} exact The additional threshold parameters when the {@code Exact} match level is used.
+     * @param {ExactMatchSettings|null} exact - The additional threshold parameters when the {@code Exact} match level is used.
      */
     ImageMatchSettings.prototype.setExact = function (exact) {
         this._exact = exact;
--- packages/eyes.sdk/src/PositionProvider.js
+++ packages/eyes.sdk/src/PositionProvider.js
@@ -3,24 +3,24 @@
 
     /**
      * @constructor
-     **/
+     */
     function PositionProvider() {}
 
     /**
      *
-     * @return {Promise<{x: number, y: number}>} The current position, or {@code null} if position is not available.
+     * @return {Promise<{x: number, y: number}>} - The current position, or {@code null} if position is not available.
      */
     PositionProvider.prototype.getCurrentPosition = function () {};
 
     /**
      * Go to the specified location.
-     * @param {{x: number, y: number}} location The position to set.
+     * @param {{x: number, y: number}} location - The position to set.
      * @return {Promise<void>}
      */
     PositionProvider.prototype.setPosition = function (location) {};
 
     /**
-     * @return {Promise<{width: number, height: number}>} The entire size of the container which the position is relative to.
+     * @return {Promise<{width: number, height: number}>} - The entire size of the container which the position is relative to.
      */
     PositionProvider.prototype.getEntireSize = function () {};
 
@@ -30,7 +30,7 @@
     PositionProvider.prototype.getState = function () {};
 
     /**
-     * @param {Object} state The initial state of position
+     * @param {Object} state - The initial state of position
      * @return {Promise<void>}
      */
     PositionProvider.prototype.restoreState = function (state) {};
